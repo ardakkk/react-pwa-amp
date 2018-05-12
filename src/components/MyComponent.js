136cb91e0172552f683ef694a8817f6498a24f41
ardakkk <a.tileu@infin-it.kz>
2018-05-12 15:34:41 +0600

    react pwa and amp

---
+++ src/components/MyComponent.js
@@ -0,0 +1,7 @@
+/*** src/index.js   ***/
+import React from 'react';
+import '../style/styles.css';
+const MyComponent = () => (
+    <h1>Hello from My Component</h1>
+);
+export default MyComponent;
